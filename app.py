540173066d21e9f8c7c573849fb4f09c6d99ccc3
Wesley Xavier <wesley.xvier@gmail.com>
2024-12-02 12:26:38 -0300

    :books: chore: continuous update of files.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home_page():
-    return('Hello World!')
+@app.route('/')
+def index():
+    name = request.args.get('name')
+    return render_template('index.html', output=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ autorun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# script de automatização de criação do ambiente virtual e instalação do flask
+
+if [ -d "venv" ]; then
+    echo "Activating virtual environment"
+    source venv/bin/activate
+else
+    echo "Creating virtual environment"
+    python3 -m venv venv
+    source venv/bin/activate
+    echo "Installing Flask"
+    pip install flask
+fi
+
+echo "Starting Flask server ..."
+flask run --host=0.0.0.0
+
+clear
+echo "Deactivating virtual environment"
+deactivate
